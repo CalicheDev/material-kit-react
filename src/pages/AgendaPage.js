d2586ffba7511202a96cc780ba8dae3e7071decb
Carlos Bejarano <cabejarano21@gmail.com>
2023-01-26 16:30:32 -0500

    feat: update front Cita-Agenda and header

--- src/pages/AgendaPage.js
+++ src/pages/AgendaPage.js
@@ -87,23 +87,15 @@ export default function Checkout() {
       <Helmet>
         <title> Cita | Agenda </title>
       </Helmet>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppBar
-          position="absolute"
-          color="default"
-          elevation={0}
-          sx={{
-            position: 'relative',
-            borderBottom: (t) => `1px solid ${t.palette.divider}`,
-          }}
-        >
-          <Toolbar>
-            <Typography variant="h6" color="inherit" noWrap>
+      <Container>
+        
+          
+            {/* <Typography variant="h6" color="inherit" noWrap> */}
+            <Typography variant="h4" sx={{ mb: 5 }}>
               Administrador de citas
             </Typography>
-          </Toolbar>
-        </AppBar>
+          
+        
         <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
           <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
             <Typography component="h1" variant="h4" align="center">
@@ -147,7 +139,7 @@ export default function Checkout() {
           </Paper>
           <Copyright />
         </Container>
-      </ThemeProvider>
+      </Container>
     </>
   );
 }
